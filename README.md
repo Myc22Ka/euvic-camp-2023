c809f4c6a50ff9596e9c8b026e9942f042d5faaf
LonlehKitteh <krzysztof02gach@gmail.com>
2023-08-08 18:54:47 +0200

    setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Quick INFO
+
+## How to prepare project?
+
+1. yarn create react-app my-app --template typescript
+2. yarn add --dev --exact prettier
+3. yarn add -D eslint
+4. npm init @eslint/config
+5. yarn add -D husky
+6. yarn prepare
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/App.scss";
+import styles from "./styles/styles.module.scss";
+
+const App: React.FC = () => {
+  console.log(styles);
+  return <div className="app">hi</div>;
+};
+export default App;
